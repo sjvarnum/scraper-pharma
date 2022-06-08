7ddcfd37e944100b6705ec87b9cb844951d88120
Shane Varnum <shane.varnum@encompassmedia.dev>
2022-06-08 15:11:14 -0400

    Implemented get_data function

--- fiercebiotech.py
+++ fiercebiotech.py
@@ -1,21 +1,32 @@
 import logging
+import os
 import pandas as pd
 import random
+import requests
 import sqlite3
 import winsound
 
 from datetime import datetime
-from requests_html import HTML
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.service import Service
-from selenium.common.exceptions import *
+from dotenv import load_dotenv, dotenv_values
 from sqlalchemy import create_engine
 from time import sleep
+from urllib.parse import urlencode
+
+load_dotenv()
+
+
+def log():
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+
+    logging.basicConfig(format='%(levelname)s | %(asctime)s | %(message)s')
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    return logger
 
 
 def database_output(logger, df, database, table):
@@ -51,121 +62,44 @@ def file_output(output_filename, logger, channel, df):
     logger.info(f'{filename} created')
 
 
-def log():
-    """sumary_line
+def get_data(urls, base_url):
+    scraperapi_key = os.getenv('scraperapi_key')
 
-    Keyword arguments:
-    argument -- description
-    Return: return_description
-    """
+    article_uuids = []
+    for url in urls: 
+        params = {'api_key': scraperapi_key, 'url': url}
+        response = requests.get('http://api.scraperapi.com/', params=urlencode(params))
+        article_uuids.extend([i['uuid'] for i in response.json()['articles']])
 
-    logging.basicConfig(format='%(levelname)s | %(asctime)s | %(message)s')
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    return logger
+    article_urls = [f'{base_url}{i}' for i in article_uuids]    
+    article_list = []
+    for article_url in article_urls:
+        sleep(random.randint(1, 5))
+        params = {'api_key': scraperapi_key, 'url': url}
+        response = requests.get('http://api.scraperapi.com/', params=urlencode(params))
+        article_list.append(response.json())
+
+    return article_list
+
+
+
+
 
 
 def main(sources, output_filename, database, table):
-    """sumary_line
-
-    Keyword arguments:
-    argument -- description
-    Return: return_description
-    """
-
     logger = log()
-    service = Service('geckodriver.exe')
-    options = webdriver.FirefoxOptions()
-    options.add_argument('-headless')
 
-    driver = webdriver.Firefox(service=service, options=options)
-    for item in sources:
-        base_url = item['base_url']
-        url = item['url']
-        channel = item['channel']
-        pages = item['pages']
-        source = item['source']
-        logger.info(f'Opening {url}')
-        driver.get(url)
-        sleep(random.randint(10, 20))
+    list_of_urls = ['https://www.fiercebiotech.com/api/v1/fronts/3961?page=1']
 
-        logger.info(
-            'Scroll to bottom of page to find "see more articles" button')
-        see_more_link = driver.find_element(By.LINK_TEXT, 'See more articles')
-        driver.execute_script(
-            'arguments[0].scrollIntoView(true);', see_more_link)
-        sleep(5)
+    article_uuids = []
+    for url in list_of_urls: 
+        params = {'api_key': scraperapi_key, 'url': url}
+        response = requests.get('http://api.scraperapi.com/', params=urlencode(params))
+        print(response.text)
 
-        for page in range(1, pages):
-            logger.info(f'Begin parse page {page}')
-            sleep(random.randint(5, 10))
-            logger.info('Checking for pop up')
-            try:
-                close_button = driver.find_element(
-                    By.CSS_SELECTOR, '.close-persistent-bar').click()
-                logger.info('Pop up found - closing it')
-                sleep(5)
-                driver.find_element(By.LINK_TEXT, 'See more articles').click()
-            except:
-                logger.info('No pop up detected - continuing')
-                driver.find_element(By.LINK_TEXT, 'See more articles').click()
+        article_uuids.extend([i['uuid'] for i in response.json()['articles']])
 
-            logger.info(
-                'Scroll to bottom of page to find "see more articles button"')
-            sleep(5)
-            see_more_link = driver.find_element(
-                By.LINK_TEXT, 'See more articles')
-            logger.info('Click the "see more articles button"')
-            driver.execute_script(
-                'arguments[0].scrollIntoView(true);', see_more_link)
-            see_more_link.click()
-            sleep(5)
-            n = random.randint(1, 5)
-            sleep(n)
 
-    body = driver.page_source
-    html = HTML(html=body)
-    articles = html.find('article.node-listing.prosumer.row')
-    article_list = []
-    for article in articles:
-        try:
-            title = article.find('.element-title.small', first=True).text
-            # print(title)
-        except:
-            None
-        try:
-            link = url[:-1] + \
-                article.find('.element-title.small > a',
-                             first=True).links.pop()
-            # print(link)
-        except:
-            None
-        try:
-            date = article.find('span.date.d-inline-block', first=True).text
-            date = datetime.strptime(
-                date[:-2], '%b %d, %Y %H:%M').strftime('%Y-%m-%d')
-            # print(date)
-        except:
-            None
-        try:
-            channel = article.find(
-                '.label.label-small > span > a', first=True).text
-            # print(channel)
-        except:
-            None
-
-        data = {
-            'date': date,
-            'source': 'www.fiercebiotech.com',
-            'channel': channel,
-            'title': title,
-            'link': link
-        }
-
-        article_list.append(data)
-
-    logger.info('Closing browser instance')
-    driver.close()
 
     df = pd.DataFrame(article_list)
     df.channel = df.channel.str.lower()
@@ -181,7 +115,10 @@ def main(sources, output_filename, database, table):
 
 if __name__ == '__main__':
 
-    num_of_pages = 3
+    list_of_urls = ['https://www.fiercebiotech.com/api/v1/fronts/3961?page=1',
+                    'https://www.fiercebiotech.com/api/v1/fronts/3961?page=2']
+
+    # num_of_pages = 3
     sources = [{
         'base_url': 'https://www.fiercebiotech.com',
         'url': 'https://www.fiercebiotech.com',
@@ -194,11 +131,11 @@ if __name__ == '__main__':
     main(sources=sources, output_filename='FierceBiotech_Articles',
          database='db', table='fiercebiotech_articles')
 
-    freq = 100
-    dur = 50
+    # freq = 100
+    # dur = 50
     
-    # loop iterates 5 times i.e, 5 beeps will be produced.
-    for i in range(0, 5):    
-        winsound.Beep(freq, dur)    
-        freq+= 100
-        dur+= 50
+    # # loop iterates 5 times i.e, 5 beeps will be produced.
+    # for i in range(0, 5):    
+    #     winsound.Beep(freq, dur)    
+    #     freq+= 100
+    #     dur+= 50
